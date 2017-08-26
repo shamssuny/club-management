f53e3ad79775815ec0b9d5d6103195faebe50b73
shamssuny <shams.suny@gmail.com>
2017-08-26 12:40:24 +0600

    nothing

--- inc/config.php
+++ inc/config.php
@@ -2,7 +2,7 @@
 	//this is used for configure the whole application. Do not delete this . 
 	// use HOST as hostname , DB for database name , user for username and password for database password 
 	define("HOST", "localhost");
-	define("DB", "project");
+	define("DB", "finalproject");
 	define("USER", "root");
 	define("PASS", "");
 ?>
